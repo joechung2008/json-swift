8b45c81d23da7222211e602949b550fe9231c04a
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-22 23:03:30 -0400

    Add more string tests

--- Tests/JsonCoreTests/StringTests.swift
+++ Tests/JsonCoreTests/StringTests.swift
@@ -29,4 +29,105 @@ public final class StringTests: XCTestCase {
     }
     XCTAssertEqual(token.value, "line\nbreak")
   }
+
+  func testParseEscapedQuote() throws {
+    let result = try JsonCore.parse("\"foo\\\"bar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\"bar")
+  }
+
+  func testParseEscapedBackslash() throws {
+    let result = try JsonCore.parse("\"foo\\\\bar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\\bar")
+  }
+
+  func testParseEscapedSlash() throws {
+    let result = try JsonCore.parse("\"foo\\/bar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo/bar")
+  }
+
+  func testParseEscapedBackspace() throws {
+    let result = try JsonCore.parse("\"foo\\bbar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\u{8}bar")
+  }
+
+  func testParseEscapedFormfeed() throws {
+    let result = try JsonCore.parse("\"foo\\fbar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\u{c}bar")
+  }
+
+  func testParseEscapedCarriageReturn() throws {
+    let result = try JsonCore.parse("\"foo\\rbar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\rbar")
+  }
+
+  func testParseEscapedTab() throws {
+    let result = try JsonCore.parse("\"foo\\tbar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "foo\tbar")
+  }
+
+  func testParseUnicode() throws {
+    let result = try JsonCore.parse("\"foo\\u0041bar\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "fooAbar")
+  }
+
+  func testParseMultipleEscapes() throws {
+    let result = try JsonCore.parse("\"\\\"\\\\\\/\\b\\f\\n\\r\\t\\u0041\"")
+    guard let token = result.token as? StringToken else {
+      XCTFail("Token is not a StringToken")
+      return
+    }
+    XCTAssertEqual(token.value, "\"\\/\u{8}\u{c}\n\r\tA")
+  }
+
+  func testParseInvalidEscape() throws {
+    XCTAssertThrowsError(try JsonCore.parse("\"foo\\xbar\""))
+  }
+
+  func testParseIncompleteUnicode() throws {
+    XCTAssertThrowsError(try JsonCore.parse("\"foo\\u12\""))
+  }
+
+  func testParseInvalidUnicode() throws {
+    XCTAssertThrowsError(try JsonCore.parse("\"foo\\uZZZZ\""))
+  }
+
+  func testParseUnterminatedString() throws {
+    XCTAssertThrowsError(try JsonCore.parse("\"foo"))
+  }
+
+  func testParseStringWithNewline() throws {
+    XCTAssertThrowsError(try JsonCore.parse("\"\nfoo\""))
+  }
 }
