5548094fcaa86ff91d8b35acdbc71df6e25c830c
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-24 01:35:24 -0400

    Wrap CLI code in main() function

--- Sources/cli/main.swift
+++ Sources/cli/main.swift
@@ -1,17 +1,21 @@
 import Foundation
 import JsonCore
 
-do {
-  // Read input from standard input, trimming trailing newlines
-  let input =
-    (String(data: FileHandle.standardInput.readDataToEndOfFile(), encoding: .utf8) ?? "")
-    .trimmingCharacters(in: .newlines)
+func main() {
+  do {
+    // Read input from standard input, trimming trailing newlines
+    let input =
+      (String(data: FileHandle.standardInput.readDataToEndOfFile(), encoding: .utf8) ?? "")
+      .trimmingCharacters(in: .newlines)
 
-  // Try parsing the input as JSON
-  let result = try JsonCore.parse(input)
+    // Try parsing the input as JSON
+    let result = try JsonCore.parse(input)
 
-  // Pretty print the parsed JSON result
-  prettyPrint(result.token)
-} catch {
-  print("Error: \(error)")
+    // Pretty print the parsed JSON result
+    prettyPrint(result.token)
+  } catch {
+    print("Error: \(error)")
+  }
 }
+
+main()
